6e4d0887289be6776845cc8e9a26fa94340a3667
Hugo Melo <hmelo7810@gmail.com>
2026-09-08 21:01:51 -0300

    Adicionar arquivos de projeto.

---
+++ ZombieSurvivor/Program.cs
@@ -0,0 +1,42 @@
+﻿// Creating variables player stats
+int life = 100;
+int hunger = 100;
+int thirst = 100;
+int day = 1;
+const int objectiveDays = 30;
+bool isAlive = true;
+bool run = true;
+
+enum Actions 
+{
+    Run,
+    Explore,
+    Eat,
+    Drink
+}
+
+enum ExplorationEvents
+{
+    NothingFound,
+    Food,
+    Drink,
+    MedicalKit
+}
+
+enum Foods
+{
+    Fruit,
+    Hamburguer,
+    CornFlakes,
+    RottenMeat,
+    Meat,
+    pasta
+}
+
+enum Drinks
+{
+    Water,
+    Soda,
+    Juice,
+    Vodka
+}
